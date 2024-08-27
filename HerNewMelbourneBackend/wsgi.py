f14107e6a644068f92faddbb1d076803d20a225b
CCA2056 <2501850253@qq.com>
2024-08-27 20:40:25 +1000

    fix bugs

--- HerNewMelbourneBackend/wsgi.py
+++ HerNewMelbourneBackend/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'choroplethmap_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'HerNewMelbourneBackend.settings'
+settings_module = 'HerNewMelbourneBackend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'HerNewMelbourneBackend.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
