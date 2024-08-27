3bb7b0e0140475cbc72d8263d6f84feb1c9ab949
CCA2056 <2501850253@qq.com>
2024-08-28 01:42:51 +1000

    change allowed host

--- HerNewMelbourneBackend/wsgi.py
+++ HerNewMelbourneBackend/wsgi.py
@@ -4,6 +4,6 @@ from django.core.wsgi import get_wsgi_application
 
 settings_module = 'HerNewMelbourneBackend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'HerNewMelbourneBackend.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HerNewMelbourneBackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
